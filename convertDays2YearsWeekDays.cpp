1514ebe4552bedaddccf491d0dee87aab8ca5f4a
Rahul Kumar <rahul.kr0324@gmail.com>
2021-10-04 15:31:22 +0530

    Create convertDays2YearsWeekDays.cpp

---
+++ convertDays2YearsWeekDays.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int y,d,w;
+
+    cout<<"Enter No. of days :: ";
+    cin>>d;
+
+    y=d/365;
+    d=d%365;
+    w=d/7;
+    d=d%7;
+
+    cout<<"\nNo. of Years: : "<<y<<"\nNo. of Weeks :: "<<w<<"\nNo. of Days :: "<<d<<"\n";
+
+    return 0;
+}
