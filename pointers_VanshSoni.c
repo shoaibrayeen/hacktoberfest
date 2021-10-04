b2ee2ae26ffe07b16958ce7acba79fccb3eb21bb
Vansh Soni <vanshsoni16@gmail.com>
2021-10-04 09:30:53 +0530

    Create pointers_VanshSoni.c

---
+++ pointers_VanshSoni.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+
+   int  abc;
+   char xyz[10];
+
+   printf("Address of var1 variable: %x\n", &abc  );
+   printf("Address of var2 variable: %x\n", &xyz  );
+
+   return 0;
+}
