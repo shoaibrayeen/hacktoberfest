1950d7570f212c647939e042238f35b0de50b787
harpreet1602 <harpreets1602@gmail.com>
2021-10-06 17:10:21 +0530

    added Stack Using Queues problem

---
+++ Java/StackUsingQueues_harpreet1602.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class StackUsingQueues_harpreet1602{
+    LinkedList<Integer> q1 = new LinkedList<>();
+    LinkedList<Integer> q2 = new LinkedList<>();
+    
+    
+    
+    public StackUsingQueues_harpreet1602(){
+
+    }
+    public void swap(LinkedList<Integer> first, LinkedList<Integer> second){
+        while(first.size()!=1){
+            second.addLast(first.removeFirst());
+        }
+    }
+    public void swap2(LinkedList<Integer> first, LinkedList<Integer> second){
+        while(first.size()!=0){
+           second.addLast(first.removeFirst()); 
+        }
+    }
+    public void push(int x) {
+        q1.addLast(x);
+    }
+    
+    public int pop() {
+        swap(q1,q2);
+        int val = q1.removeFirst();    
+        swap2(q2,q1);
+        return val;
+    }
+    
+    public int top() {
+    swap(q1,q2);
+    int val = q1.getFirst(); 
+    q2.addLast(q1.removeFirst());
+    swap2(q2,q1);  
+    return val;
+    }
+    
+    public boolean empty() {
+        return q1.size()==0;
+    }
+
+    
+}
---
+++ Java/client.java
@@ -0,0 +1,10 @@
+public class client {
+    public static void main(String[] args){
+     StackUsingQueues_harpreet1602 st =new StackUsingQueues_harpreet1602();
+     st.push(10);
+     st.push(20);
+     st.push(30);
+     System.out.println(st.pop());
+     System.out.println(st.top());
+    }
+}
